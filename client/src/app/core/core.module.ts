eca90489b844a02f2fd37675ff9ce222b1b67f8a
juanicortesl-forcast <juan.cortes@forcast.cl>
2022-02-15 19:10:23 -0300

    new meeting styles

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,10 +7,30 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { MeetingCardComponent } from './components/meeting-card/meeting-card.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
-  exports: [MeetingCardComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MeetingCardComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   providers: [HttpClient],
   declarations: [MeetingCardComponent, ShortenPipe],
 })
--- client/src/app/modules/dashboard/pages/meetings/meetings.component.ts
+++ client/src/app/modules/dashboard/pages/meetings/meetings.component.ts
@@ -1,6 +1,13 @@
+import { MatNativeDateModule } from '@angular/material/core';
 import { ApiService } from '../../../../core/http/api.service';
 import { Component, OnInit } from '@angular/core';
-import { dateInputsHaveChanged } from '@angular/material/datepicker/datepicker-input-base';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-meetings',
@@ -8,71 +15,82 @@ import { dateInputsHaveChanged } from '@angular/material/datepicker/datepicker-i
   styleUrls: ['./meetings.component.scss'],
 })
 export class MeetingsComponent implements OnInit {
-  public searcherUser: boolean = false;
-  nextMeetings: any[] = [];
-  offeredMeetings: any[] = [];
-  displayNewMeeting = 'none';
-  newMeetingDate: any = {};
-  newMeetingTime = { hour: 12, minute: 0 };
-  pastMeetings: any[] = [];
-  public today: Date = new Date();
+  step = 6;
+  public firstStepForm = new FormGroup({
+    type: new FormControl('', [Validators.required, Validators.minLength(1)]),
+  });
+  public secondStepForm = new FormGroup(
+    {
+      theme: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+      otherTheme: new FormControl('', []),
+    },
+    {
+      validators: [MeetingsComponent.isThemeValid],
+    }
+  );
+  public thirdStepForm = new FormGroup({
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(300),
+    ]),
+  });
+  public fourthStepForm = new FormGroup({
+    date: new FormControl('', [Validators.required]),
+  });
+
+  minDate: Date | undefined;
+  maxDate: Date | undefined;
+  public fifthStepForm = new FormGroup({
+    startTime: new FormControl('', [Validators.required]),
+    endTime: new FormControl('', [Validators.required]),
+  });
+
+  public sixthStepForm = new FormGroup({
+    acceptedTerms: new FormControl('', [Validators.required]),
+  });
+  static isThemeValid(control: AbstractControl): ValidationErrors | null {
+    if ((control.get('theme')?.value as string) !== 'other') {
+      return null;
+    }
+    if ((control.get('otherTheme')?.value as string).length < 3) {
+      return { isThemeValid: false };
+    }
+    return null;
+  }
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.searcherUser = localStorage.getItem('userType') === 'searcher';
-    this.loadMeetings();
+    this.minDate = new Date();
+    this.maxDate = new Date();
+    this.maxDate.setDate(this.maxDate.getDate() + 30);
   }
-  openNewMeeting() {
-    this.displayNewMeeting = 'block';
-    console.log(this.newMeetingDate.day);
-  }
+
   createNewMeeting() {
     try {
-      let newDate = new Date(
-        this.newMeetingDate.year,
-        this.newMeetingDate.month - 1,
-        this.newMeetingDate.day,
-        this.newMeetingTime.hour,
-        this.newMeetingTime.minute
-      );
-      this.apiService.addAvailableMeeting({ date: newDate }).subscribe({
+      this.apiService.addAvailableMeeting({}).subscribe({
         next: (data: any) => {
           console.log(data);
-          this.closeNewMeeting();
-          this.loadMeetings();
         },
         error: (error: any) => {
-          this.closeNewMeeting();
           console.log(error);
         },
       });
     } catch {}
-    console.log(this.newMeetingDate);
   }
-  closeNewMeeting() {
-    this.displayNewMeeting = 'none';
+
+  nextStep() {
+    this.step++;
   }
-  loadMeetings() {
-    this.apiService.getNextMeetings().subscribe((data: any) => {
-      console.log(data);
-      if (data.result) {
-        this.nextMeetings = data.data.model;
-      }
-      // this.nextMeetings.forEach((meeting) => {
-      //   meeting.other = meeting.offerer ? meeting.Offerer : meeting.Searcher;
-      // });
-    });
-    this.apiService.getOfferedMeetings().subscribe((data: any) => {
-      console.log(data);
-      if (data.result) {
-        this.offeredMeetings = data.data.model;
-      }
-    });
-    this.apiService.getPastMeetings().subscribe((data: any) => {
-      console.log(data);
-      if (data.result) {
-        this.pastMeetings = data.data.model;
-      }
-    });
+
+  previousStep() {
+    this.step--;
+  }
+
+  get theme() {
+    return this.secondStepForm.get('theme')?.value;
   }
 }
